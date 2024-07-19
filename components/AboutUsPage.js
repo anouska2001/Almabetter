ddc5fe0a9a80f5508bc79a1d09a7ba6949d53ed9
anouska2001 <blockunblock13@gmail.com>
2024-07-19 20:28:41 +0530

    Create AboutUsPage.js

---
+++ components/AboutUsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutUsPage = () => {
+return (
+<div>
+<header>
+{/* Logo and navigation links */}
+</header>
+<h1>About Us</h1>
+<p>Information about our website and team.</p>
+</div>
+);
+};
+
+export default AboutUsPage;
