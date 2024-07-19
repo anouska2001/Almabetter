fa47d986786573aea4f29bad7d7e5f3c6ab5a14b
anouska2001 <blockunblock13@gmail.com>
2024-07-19 20:26:32 +0530

    Create KeySkills.js

---
+++ components/DetailsFillingSections/KeySkills.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const KeySkills = () => {
+return (
+<div>
+{/* Key Skills Form */}
+<Link to="/preview">Preview</Link>
+</div>
+);
+};
+
+export default KeySkills;
